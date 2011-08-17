cc3d4112fe9b1e6512059fc1356b83294d3b4fef
Erik Søe Sørensen <eriksoe+gh@gmail.com>
2011-08-17 15:29:11 +0200

    Client: config support.

--- client/tricycle_lib.sh
+++ client/tricycle_lib.sh
@@ -21,11 +21,45 @@ $0 resolve [<resolution>]
     Requires that the current revision was built successfully.
     <resolution> must be one of (c|w|n | closed | fixed | wontfix)
 
+$0 config
+    Print out the client's configuration.
+
 EOF
 }
 
 function bad_usage {
     usage "$@" ; exit 1
+}
+
+#==================== Configuration ========================================
+CONFIG_FILE="./.tricycle"
+function read_configuration {
+    if [ -f "$CONFIG_FILE" -a -r "$CONFIG_FILE" ] ; then
+	echo "Reading config..."
+	shopt -s extglob # Used for trimming
+	while s=""; read s || [ -n "$s" ] ; do
+	    if [[ "$s" == "#"* ]]  ; then continue; fi # Comment line
+	    if [[ "$s" != *"="* ]] ; then continue; fi # No '='
+
+	    propname=${s%%=*} ; propname=${propname%%*([[:space:]])}
+	    propvalue=${s#*=} ; propvalue=${propvalue##*([[:space:]])}
+
+	    varname="tmp_cfg_${propname}"
+	    declare "$varname"="$propvalue"
+	done < "$CONFIG_FILE"
+    fi
+
+    # Export:
+    # (Why does 'declare' make variables local? -ES)
+    cfg_server_host="$tmp_cfg_server_host"
+}
+
+function dump_configuration {
+    echo '/---- Tricycle client configuration'
+    for var in "${!cfg_*}" ; do
+	printf "%-16s = %s\n" "${var#cfg_}" `eval "echo \\$$var"`
+    done
+    echo '\---- Tricycle client configuration'
 }
 
 #==================== Communication ========================================
@@ -47,12 +81,23 @@ function show_build_status {
 #==================== Command-line parsing ==============================
 
 function perform_command {
+    # Special case: no command was given.
+    if [ $# -eq 0 ] ; then usage ; exit 0 ; fi
+
     local cmd="$1"
+    read_configuration
+
     case "$cmd" in
 	jira)
 	    case $# in
 		1) show_ticket_id ;;
 		2) set_ticket_id "$2" ;;
+		*) bad_usage
+	    esac
+	    ;;
+	config)
+	    case $# in
+		1) dump_configuration ;;
 		*) bad_usage
 	    esac
 	    ;;
